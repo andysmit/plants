6dbbb37dc4156b878c319dffb41730e56427af2b
Andy <andysmit@uvic.ca>
2021-04-15 16:40:01 -0700

    Bridged the database with the backend server

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -15,12 +17,11 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter(file =>
-    (file.indexOf('.') !== 0) &&
-    (file !== basename) &&
-    (file.slice(-3) === '.js'))
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
   .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
--- api/routes/plants.js
+++ api/routes/plants.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var router = express.Router();
+const Plants = require('../db/models').Plants;
 
 router.get("/", function(req, res, next) {
     res.send("Initial API Test");
 });
 
+router.post("/", function(req, res, next) {
+    return Plants
+    .create({
+      name: req.body.name,
+      description: req.body.description,
+      season: req.body.season,
+    })
+    .then(plants => res.status(201).send(plants))
+    .catch(error => res.status(400).send(error));
+});
 module.exports = router;
