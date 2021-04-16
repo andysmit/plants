3c6e23e2319c29019ab590262a10062555f497dd
Andy <andysmit@uvic.ca>
2021-04-15 17:50:18 -0700

    Added update API function in the backend

--- api/routes/plants.js
+++ api/routes/plants.js
@@ -5,9 +5,9 @@ const Plants = require('../db/models').Plants;
 router.post("/", function(req, res, next) {
     return Plants
     .create({
-      name: req.body.name,
-      description: req.body.description,
-      season: req.body.season,
+        name: req.body.name,
+        description: req.body.description,
+        season: req.body.season,
     })
     .then(plants => res.status(201).send(plants))
     .catch(error => res.status(400).send(error));
@@ -17,6 +17,23 @@ router.post("/", function(req, res, next) {
 router.get("/", function(req, res, next) {
     return Plants
     .findAll()
+    .then(plants => res.status(201).send(plants))
+    .catch(error => res.status(400).send(error));
+});
+
+router.put("/", function(req, res, next) {
+    return Plants
+    .update(
+        {
+            name: req.body.name,
+            description: req.body.description,
+            season: req.body.season,
+        },
+        {
+            where: { id: req.body.id }
+            
+        }
+    )
     .then(plants => res.status(201).send(plants))
     .catch(error => res.status(400).send(error));
 });
