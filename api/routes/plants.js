348ec611afefeadba1fa7a054caef81c98665043
Andy <andysmit@uvic.ca>
2021-04-15 17:53:51 -0700

    Added delete API function in the backend

--- api/routes/plants.js
+++ api/routes/plants.js
@@ -38,4 +38,16 @@ router.put("/", function(req, res, next) {
     .catch(error => res.status(400).send(error));
 });
 
+
+router.delete("/", function(req, res, next) {
+    return Plants
+    .destroy(
+        {
+            where: { id: req.body.id }
+        }
+    )
+    .then(plants => res.status(201).send(plants))
+    .catch(error => res.status(400).send(error));
+});
+
 module.exports = router;
