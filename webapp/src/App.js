0644e896aabeff0a949df0051e68a9f4cf9b1620
Andy <andysmit@uvic.ca>
2021-04-15 18:15:20 -0700

    Created list all function in the frontend

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,39 +1,76 @@
 import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col, Table, Form} from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
-      super(props);
-      this.state = { apiResponse: "" };
+    super(props);
+    this.state = {
+      name: "",
+      desecription: "",
+      season: "",
+      plants: []
+    };
+    this.getPlants = this.getPlants.bind(this)
   }
-
-  callAPI() {
-      fetch("http://localhost:8080/plants")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }));
-  }
-
-  componentWillMount() {
-      this.callAPI();
-  }
+  componentDidMount(){
+		this.getPlants();
+	}
+	getPlants() {
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json')
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders
+    }
+    fetch("http://localhost:8080/plants", requestOptions)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({plants: data})
+        console.log(this.state.ducks)
+      })
+      .catch(error => console.log('error', error));
+		
+		
+	}
     
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-intro">;{this.state.apiResponse}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+<Container>
+
+	<Row>
+		<Col md={{span:4, offset:4 }}>
+			<h1>Plants Data</h1>
+		</Col>
+	</Row>
+
+	<Row>
+		<Table striped bordered hover>
+			<thead>
+				<tr>
+				 <th>id</th>
+				 <th>Name</th>
+				 <th>Description</th>
+				 <th>Season</th>
+				</tr> 
+		   </thead>
+			<tbody>
+			  {this.state.plants.map((data, key) => {
+				return (
+				<tr>
+					<th>{data.id}</th>
+					<th>{data.name}</th>
+					<th>{data.description}</th>
+					<th>{data.season}</th>
+				</tr>
+				)
+			  })}
+			  </tbody>
+		  </Table>
+	  </Row>
+  </Container>
+
     );
   }
 }
